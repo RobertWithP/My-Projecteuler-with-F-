31fcac6bcd75eafa3a1be9504945015a059370be
Robert <robiswelt@googlemail.com>
2013-02-11 16:24:18 +0100

    solved Problem 1

--- ProjectEuler/ProjectEuler.Problems/Problem_1.fs
+++ ProjectEuler/ProjectEuler.Problems/Problem_1.fs
@@ -1,2 +1,19 @@
-﻿module Problem_1
+﻿namespace ProjectEuler.Problems
 
+module Problem_1 =
+    
+    let isModulo x = x % 3 = 0 || x % 5 = 0
+
+    let calculate(below:int) =        
+        [1..below - 1] |> List.filter isModulo |> List.sum
+
+    let run =
+        calculate 1000
+
+    let test =
+        calculate(10) = 23
+
+    let info =
+        printf "http://projecteuler.net/problem=1\r\n"
+        printf "Multiples of 3 and 5 \r\n"
+        printf "Solved on 11.02.2013 \r\n"
--- ProjectEuler/ProjectEuler/Program.fs
+++ ProjectEuler/ProjectEuler/Program.fs
@@ -1,7 +1,26 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-[<EntryPoint>]
+﻿[<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    
+    let test (x:bool) = 
+        if x = false then
+            let s = Printf.TextWriterFormat<unit>("\r\nError with test!\r\n")
+            printfn s
+        else
+            printfn "\r\nTest run successfully!\r\n"
+
+    let showresult (result:string) =
+        let s = Printf.TextWriterFormat<string -> unit>("Result:\r\n%s\r\n")
+        printfn s result
+
+
+    // change here problem
+    let run =
+        ProjectEuler.Problems.Problem_1.info
+        let p = ProjectEuler.Problems.Problem_1.test       
+        test (p)
+            
+        let result = ProjectEuler.Problems.Problem_1.run
+        showresult (result.ToString())
+
+    let read = System.Console.ReadLine()
     0 // return an integer exit code
