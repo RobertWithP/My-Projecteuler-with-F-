238f1483995fc433f7b183e04dbd44dab69e2d70
Robert <robiswelt@googlemail.com>
2013-02-13 13:56:52 +0100

    add Problem 6 module dummy

---
+++ ProjectEuler/ProjectEuler.Problems/Problem_006.fs
@@ -0,0 +1,18 @@
+﻿namespace ProjectEuler.Problems
+
+module Problem_6 =
+
+    let calculate x = 
+        x
+
+    let run =
+        // todo: no string to char and int converison
+        calculate ( 100 )
+        
+    let test =
+        // 2 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+        calculate ( 10 ) = 2640
+
+    let info =
+        printf "http://projecteuler.net/problem=6\r\n"
+        printf "Sum square difference \r\n"
