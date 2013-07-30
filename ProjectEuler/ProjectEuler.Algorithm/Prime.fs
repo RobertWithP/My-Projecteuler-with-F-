14e050750f5dc60b3f4c44359cf1d0df10b254a2
Robert <robiswelt@googlemail.com>
2013-07-30 14:27:27 +0200

    add prime algorithm file

---
+++ ProjectEuler/ProjectEuler.Algorithm/Prime.fs
@@ -0,0 +1,23 @@
+﻿namespace ProjectEuler.Algorithm
+
+module Prime =
+
+    let multiple start max factor = Seq.unfold (fun state -> 
+        if (state > max) then None 
+        else Some(state, state + factor)) start   
+        
+    let sieve max = 
+        let to_check = Array.create(max + 1) false
+
+        let mutable value = 0L // refactor here
+
+        for i = 2 to max do            
+            if  to_check.[i].Equals false then              
+                multiple (i * 2) max i |> Seq.iter (fun x -> to_check.[x] <- true)
+
+        // sum all prime
+        for i = 2 to max do
+            if  to_check.[i].Equals false then
+                value <- value + int64(i) 
+
+        value
--- ProjectEuler/ProjectEuler.Problems/Problem_010.fs
+++ ProjectEuler/ProjectEuler.Problems/Problem_010.fs
@@ -1,41 +1,23 @@
 ﻿namespace ProjectEuler.Problems
 
 open System
+open ProjectEuler.Algorithm
 
 module Problem_10 =   
 
-    let multiple start max factor = Seq.unfold (fun state -> 
-        if (state > max) then None 
-        else Some(state, state + factor)) start   
-        
-    let sieve max = 
-        let to_check = Array.create(max + 1) false
-
-        let mutable value = 0L // refactor here
-
-        for i = 2 to max do
-            
-            if  to_check.[i].Equals false then
-                
-                multiple (i * 2) max i |> Seq.iter (fun x -> to_check.[x] <- true)
-
-                value <- value + int64(i) 
-
-        value
-
     let test =
-        let t = sieve ( 10 ) 
+        let t = Prime.sieve ( 10 ) 
         t = 17L
 
     let run =
 
         //refactor c# -> under 50ms
 
-        //let sw = System.Diagnostics.Stopwatch()
-        //sw.Start()
-        let result = sieve ( 2000000 )    
-        //sw.Stop()
-        //printfn "%f" sw.Elapsed.TotalMilliseconds
+//        let sw = System.Diagnostics.Stopwatch()
+//        sw.Start()
+        let result = Prime.sieve ( 2000000 )    
+//        sw.Stop()
+//        printfn "%f" sw.Elapsed.TotalMilliseconds
     
         result
 
